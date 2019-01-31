033da1eec63d034988600c883a9f694e4deee931
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-01-31 17:47:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ app.use(express.static('public'));
 
 app.get('/node/', function (req, res) {
   const ipInfo = req.ipInfo;
-  var message = 'your IP is: ' + req.connection.remoteAddress;
-  res.send(message);
+  //var message = 'your IP is: ' + req.connection.remoteAddress;
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  res.send(ip);
 })
 
 var server = app.listen(3000, function () {
