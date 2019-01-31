c40cdf36159ea934eb73267305c2235fd40c8ff3
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-01-31 17:37:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 app.get('/node/', function (req, res) {
   const ipInfo = req.ipInfo;
-  var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
+  var message = 'your IP is: ' + req.connection.remoteAddress`;
   res.send(message);
 })
 
