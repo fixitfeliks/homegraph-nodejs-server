0550df11b165e732429fc509df8080ca20148e57
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-01-30 22:33:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
     res.send(message);
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
     var host = server.address().address
     var port = server.address().port
 
