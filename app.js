cbbb6c54ea84f87046dc38bd09f9b05f701b611e
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-01-31 17:35:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
+var expressip = require('express-ip');
 
 var app = express();
 
+app.use(expressip().getIpInfoMiddleware);
 app.use(express.static('public'));
 
 
 app.get('/node/', function (req, res) {
-    var message = req.headers;
-    res.send(message);
+  const ipInfo = req.ipInfo;
+  var message = `Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`;
+  res.send(message);
 })
 
 var server = app.listen(3000, function () {
-    var host = server.address().address
     var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Example app listening on port:%s", port)
 })
