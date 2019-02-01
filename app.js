c40b85ab10d8e383fa9a72c5163c3bdac90edba7
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-01-31 22:34:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/node/', function (req, res) {
   //var message = 'your IP is: ' + req.connection.remoteAddress;
   //var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
   //res.send(JSON.stringify(req.headers,null,4) + "\n" + JSON.stringify(geoip.allData(req.headers["x-real-ip"]),null, 4));
-  res.render('node.html',{"IP":req.headers["x-real-ip"],"user-agent":req.headers["user-agent"],"referer":req.headers["Referer"]});
+  res.render('node.html',{"IP":req.headers["x-real-ip"],"user-agent":req.headers["user-agent"],"referer":req.headers.referer});
  // var geoSON = JSON.stringify(geoip.allData(req.headers["x-real-ip"]).code);
  // res.send(geoSON);
 })
