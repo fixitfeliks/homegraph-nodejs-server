e470c15abf56c31cc8ff50f0368256c271743d60
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-01-31 19:24:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 var express = require('express');
-var expressip = require('express-ip');
 var geoip = require('geo-from-ip');
+var mustacheExpress = require('mustache-express');
 
 var app = express();
 
-app.use(expressip().getIpInfoMiddleware);
-app.use(express.static('public'));
+app.engine('html', mustacheExpress());
+app.set('view agent','html');
+app.set('views',__dirname + 'public');
 
+app.use(express.static('public'));
 
 app.get('/node/', function (req, res) {
   //const ipInfo = req.ipInfo;
   //var message = 'your IP is: ' + req.connection.remoteAddress;
   //var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-  res.send(JSON.stringify(req.headers,null,4) + "\n" + JSON.stringify(geoip.allData(req.headers["x-real-ip"]),null, 4));
+  //res.send(JSON.stringify(req.headers,null,4) + "\n" + JSON.stringify(geoip.allData(req.headers["x-real-ip"]),null, 4));
+  res.render('index.html',{"object":"HIII"});
 })
 
 var server = app.listen(3000, function () {
