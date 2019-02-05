c9b8979fd834a8fbf686d658a747e2aaae4eb9ff
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-02-05 00:34:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get('/dynamo', function(req,res) {
     if (err) {
         res.send(JSON.stringify(err));
     } else {
-         doClient.scan(params);
+         doClient.scan(params2, function(err,data) {res.send("HI")});
     }
   });
 });
