e47f2fd418b7c6e2d2d2e28aa95a7fd0bd426452
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-02-05 18:44:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.get('/dynamo', function(req,res) {
   var params2 = {  
     TableName:"visitor_log",
     ScanIndexForward: "false",
+    Items:10,
     KeyConditionExpression: "#type = :tttt",
     ExpressionAttributeNames:{
         "#type": "data_type"
