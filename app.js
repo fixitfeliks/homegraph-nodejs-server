8b9e1a2796173bbbb0adb9b18ec1454811c01b3c
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-02-05 21:37:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var AWS = require('aws-sdk');
 
 AWS.config.update({
   region: "us-east-2",
-  endpoint: "https://dynamodb.us-east-2.amazonaws.com"
+  endpoint: "52.94.4.0/24"
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
