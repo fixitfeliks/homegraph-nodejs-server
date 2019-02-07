b1fb6b5e966bbdc7f6a3570ed9abf134ab0433c9
flexBombs <44269843+flexBombs@users.noreply.github.com>
2019-02-06 22:11:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -76,5 +76,5 @@ app.get('/dynamo', function(req,res) {
 var server = app.listen(3000, function () {
     var port = server.address().port
 
-    console.log("Example app listening on port:%s", port)
+    console.log("listening on port...%s", port)
 });
