127c1e52dfd9be605a3a79466b9c79e5bd00e2ab
Felix <felixp1337@gmail.com>
2019-03-04 15:15:58 -0500

    srsly

--- app.js
+++ app.js
@@ -197,8 +197,7 @@ function handleAuthCode(req, res) {
     console.error('expired code');
     return res.status(400).send('expired code');
   }
-
-  if (clientId != process.env.GOOGLE_CLIENT_ID) {
+  if (clientId != process.env.GOOGLE_REQ_ID) {
     console.error('invalid code - wrong client');
     return res.status(400).send('invalid code - wrong client');
   }
