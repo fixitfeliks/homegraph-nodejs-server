54235765a95e489137f5fdd07daac25e47cc4f61
Felix <felixp1337@gmail.com>
2019-03-04 19:33:16 -0500

    added light device back in

--- app.js
+++ app.js
@@ -322,8 +322,8 @@ function handleAuthCode(req, res) {
             devices: []
           }
         };
-        console.log('sync response', JSON.stringify(setDevice('',reqdata)));
-        res.status(200).json(setDevice('',reqdata));
+        console.log('sync response', JSON.stringify(setDevice(lightDevice,reqdata)));
+        res.status(200).json(setDevice(lightDevice,reqdata));
         break;
       }
     }
